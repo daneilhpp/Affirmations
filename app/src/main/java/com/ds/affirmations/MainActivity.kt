dbff0d87e7d58f52e8bb2bb6c5187f0eced8bfe2
daneilhpp <carvalhosilvad88@gmail.com>
2022-05-04 21:42:41 -0300

    [Commit01] Initial Commit

---
+++ app/src/main/java/com/ds/affirmations/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ds.affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.ds.affirmations.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val textView: TextView = findViewById(R.id.affirmationssize)
+        textView.text = Datasource().loadAffirmation().size.toString()
+    }
+}
---
+++ app/src/main/java/com/ds/affirmations/model/Affirmation.kt
@@ -0,0 +1,4 @@
+package com.ds.affirmations.model
+
+data class Affirmation(val stringResourceId: Int) {
+}
